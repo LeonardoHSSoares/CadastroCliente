8d8bcf4849e6c624daa87e2f32b28d69afd75a6c
LeonardoHSSoares <mascoty2005@gmail.com>
2024-01-10 18:57:02 -0300

    🔧 build:

--- src/resource/domain/Cliente.java
+++ src/resource/domain/Cliente.java
@@ -9,6 +9,49 @@ public class Cliente {
     private Integer numCasa;
     private String cidade;
     private String estado;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Long getCpf() {
+        return cpf;
+    }
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+    public Long getTel() {
+        return tel;
+    }
+    public void setTel(Long tel) {
+        this.tel = tel;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public Integer getNumCasa() {
+        return numCasa;
+    }
+    public void setNumCasa(Integer numCasa) {
+        this.numCasa = numCasa;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 
 
 }
