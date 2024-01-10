4af3350d805c60e02a76f9f6895753977c56a6c7
LeonardoHSSoares <mascoty2005@gmail.com>
2024-01-10 18:34:53 -0300

    init

--- src/resource/domain/Cliente.java
+++ src/resource/domain/Cliente.java
@@ -1,4 +1,14 @@
 package resource.domain;
 
 public class Cliente {
+
+    private String nome;
+    private Long cpf;
+    private Long tel;
+    private String endereco;
+    private Integer numCasa;
+    private String cidade;
+    private String estado;
+
+
 }
